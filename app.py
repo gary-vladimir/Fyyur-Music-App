d73c8cd4482c443032440174840e008cbb01bfb1
Gary Vladimir <gary@garybricks.com>
2024-06-01 14:01:06 -0600

    fixed bug regarding python-dateutil

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ moment = Moment(app)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-# TODO: connect to a local postgresql database
 
 # ----------------------------------------------------------------------------#
 # Models.
@@ -32,48 +31,49 @@ migrate = Migrate(app, db)
 
 class Venue(db.Model):
     __tablename__ = "Venue"
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
+    phone = db.Column(db.String(120), nullable=False)
     website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
+
     shows = db.relationship("Show", backref="venue", lazy=True)
 
 
 class Artist(db.Model):
     __tablename__ = "Artist"
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
 
     shows = db.relationship("Show", backref="artist", lazy=True)
 
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = "Show"
 
     id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+
     venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
 
 
 # ----------------------------------------------------------------------------#
